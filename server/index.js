627f6122679c5fe19d8139a6f940c5ad7253f0f1
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2019-09-19 20:03:31 +0200

    deleting unnecessary files

--- server/index.js
+++ server/index.js
@@ -8,14 +8,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(pino);
 
-app.get('/api/greeting', (req, res) => {
-    const name = req.query.name || 'World';
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
-        greeting: `Hello ${name}!`
-    }));
-});
-
 app.listen(3001, () =>
     console.log('Express server is running on localhost:3001')
 );
