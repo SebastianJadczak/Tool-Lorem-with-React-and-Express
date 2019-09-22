d41cb4de771341519953b7f65e855ba84f994e42
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2019-09-22 15:23:15 +0200

    add pages to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import Header from './pages/Header';
+import Main from './pages/Main';
+import Navigation from './Navigation';
+import Footer from './pages/Footer'
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hej!</p>
+        <Header />
       </header>
+      <nav>
+        <Navigation />
+      </nav>
+      <main>
+        <Main />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
